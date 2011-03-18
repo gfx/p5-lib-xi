23d1af2b58b36f5b6f7d3de0544c3209c3d8cbab
Fuji, Goro <gfuji@cpan.org>
2011-03-18 21:10:00 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 69bc2df..7586ed7 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension lib::xi
    
    -0.01 <<SET RELEASE DATE HERE>>
    -    - original version; created by /home/gfx/perl5/perlbrew/perls/perl-5.12.2/bin/dim
    +0.02 2011-03-18 21:09:31
    +    - Fix the case where $install_dir is omitted
    +
    +0.01 2011-03-18 11:09:14
    +    - original version; created by dim
           at Fri Mar 18 10:52:22 2011.

--- lib/lib/xi.pm
+++ lib/lib/xi.pm
@@ -3,7 +3,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp        ();
 use Config      ();
@@ -78,7 +78,7 @@ lib::xi - Installs missing modules on demand
 
 =head1 VERSION
 
-This document describes lib::xi version 0.01.
+This document describes lib::xi version 0.02.
 
 =head1 SYNOPSIS
 
