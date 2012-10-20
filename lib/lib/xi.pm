5b5c7339938e7d22407e8b40e11b7d493407065a
Fuji, Goro <g.psy.va@gmail.com>
2012-10-20 21:04:45 +0900

    Checking in changes prior to tagging of version 0.13.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e312a56..19636ac 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension lib::xi
    
    +0.13 2012-10-20 21:04:29
    +    - ignore Log::Agent, used in Storable
    +    - Upgraded Module::Install.
    +
     0.12 2011-10-25 17:28:16
         - Give a module name to cpanm, not a file name

--- lib/lib/xi.pm
+++ lib/lib/xi.pm
@@ -3,7 +3,7 @@ use 5.008_001;
 use strict;
 use warnings FATAL => 'all';
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 use Config ();
 
@@ -101,7 +101,7 @@ lib::xi - Installs missing modules on demand
 
 =head1 VERSION
 
-This document describes lib::xi version 0.12.
+This document describes lib::xi version 0.13.
 
 =head1 SYNOPSIS
 
